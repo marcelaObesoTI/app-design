2e12afd600c225e2de8702507d36aae603ac8f01
marcelaobeso <marcelaobeso@MBGTITO978B.local>
2022-10-11 14:53:00 -0600

    removed features from template and added simple color changer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect} from "react";
+
 
 function App() {
+  const colorPicker = () => {
+    return `#${Math.random().toString(16).substr(-6)}`;
+  };
+  let newColor = colorPicker();
+  const [color, setColor] = useState('#6cebe2');
+  useEffect(() => {
+    document.body.style.backgroundColor = color
+  },[color])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+        <h1>Miaw there</h1>
+        <button onClick={()=>{setColor(newColor)}}>Change background</button>
       </header>
     </div>
   );
