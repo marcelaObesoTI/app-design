12390a63bda35d1e969a07b9e8f8f80edae467ee
marcelaObesoTI <114107630+marcelaObesoTI@users.noreply.github.com>
2022-10-18 14:06:37 -0600

    Feature develop formating (#9)
    * feat: added eslin, prettier, commitlint and husky
    
    * feat: formated withprettier
    
    Co-authored-by: marcelaobeso <marcelaobeso@MBGTITO978B.local>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import "./App.css";
-import React, { useState, useEffect} from "react";
-
+import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const colorPicker = () => {
-    return `#${Math.random().toString(16).substr(-6)}`;
-  };
-  let newColor = colorPicker();
-  const [color, setColor] = useState('#6cebe2');
-  useEffect(() => {
-    document.body.style.backgroundColor = color
-  },[color])
+	const colorPicker = () => {
+		return `#${Math.random().toString(16).substr(-6)}`;
+	};
+	const newColor = colorPicker();
 
-  return (
-    <div className="App">
-      <header className="App-header" >
-        <h1>Miaw there</h1>
-        <button onClick={()=>{setColor(newColor)}}>Change background</button>
-      </header>
-    </div>
-  );
+	const [color, setColor] = useState('#6cebe2');
+	useEffect(() => {
+		document.body.style.backgroundColor = color;
+	}, [color]);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>Miaw there</h1>
+				<button
+					onClick={() => {
+						setColor(newColor);
+					}}
+				>
+					Change background
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
