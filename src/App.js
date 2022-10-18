071743f8b503981b3e8a07e4a900812a1459b4d3
marcelaobeso <marcelaobeso@MBGTITO978B.local>
2022-10-18 11:56:05 -0600

    feat: added contentful

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+const contentful = require('contentful');
+const client = contentful.createClient({
+	space: 'tvhoo57j3k4g',
+	accessToken: 'gKX0EqBC6lS-eqwnTLYQzVKNFt1gtKgu81QYYlLKk8A',
+});
 
-function App() {
+const App = () => {
+	const [content, setContent] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const fetchData = async () => {
+		setIsLoading(true);
+		const result = await fetchContent();
+		console.log(result);
+		setContent(result);
+		setIsLoading(false);
+	};
 	const colorPicker = () => {
 		return `#${Math.random().toString(16).substr(-6)}`;
 	};
@@ -11,21 +25,49 @@ function App() {
 	useEffect(() => {
 		document.body.style.backgroundColor = color;
 	}, [color]);
+	const fetchContent = () => client.getEntries().then(response => response);
+	/**
+	 * Make API request
+	 */
 
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<h1>Miaw there</h1>
-				<button
-					onClick={() => {
-						setColor(newColor);
-					}}
-				>
-					Change background
-				</button>
-			</header>
+			{isLoading ? (
+				<header className='App-header'>
+					<button onClick={() => fetchData()}>Ready to fetch content</button>
+				</header>
+			) : (
+				<header className='App-header'>
+					<h1>{content.items[0].fields.miawThere}</h1>
+					<button
+						onClick={() => {
+							setColor(newColor);
+						}}
+					>
+						{content.items[0].fields.button}
+					</button>
+				</header>
+			)}
 		</div>
 	);
-}
+};
+
+// function App() {
+
+// 	return (
+// 		<div className='App'>
+// 			<header className='App-header'>
+// 				<h1>Miaw there</h1>
+// 				<button
+// 					onClick={() => {
+// 						setColor(newColor);
+// 					}}
+// 				>
+// 					Change background
+// 				</button>
+// 			</header>
+// 		</div>
+// 	);
+// }
 
 export default App;
