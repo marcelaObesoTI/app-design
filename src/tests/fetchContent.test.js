7cd9e31fa8a6df92798162eadc9dbba5adcb09c6
marcelaobeso <marcelaobeso@MBGTITO978B.local>
2022-11-02 20:53:42 -0600

    feat: removed a couple unecesary imports

--- src/tests/fetchContent.test.js
+++ src/tests/fetchContent.test.js
@@ -1,8 +1,6 @@
 import { act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import fetchContent from '../fetchContent';
-import App from '../App';
-import http from '../../__mocks__/http';
 
 jest.mock('../../__mocks__/http');
 test('should return an object with content', async () => {
