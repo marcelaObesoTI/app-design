270e33af962c8c557efc3fabeaf4326a7ba9ff9d
marcelaobeso <marcelaobeso@marcela.local>
2022-11-08 02:20:57 -0600

    Updates

--- __mocks__/fakeTimer.js
+++
@@ -1,7 +0,0 @@
-function fakeTimer(callback) {
-	setTimeout(() => {
-		callback && callback();
-	}, 1000);
-}
-
-module.exports = fakeTimer;
--- src/tests/fetchContent.test.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-//import fetchContent from '../fetchContent';
-
-jest.mock('../../__mocks__/http');
-test('should return an object with content', async () => {
-	// const result = await fetchContent();
-	// expect(result.button).toBe('Change colors');
-});
